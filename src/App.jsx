7697e9237419a8feebb751a9ee6c22dfd80fb285
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-02 17:49:42 -0500

    Work on API<->Front interaction

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Score from "./assets/components/Score.jsx";
 
 function App () {
   const [count, setCount] = useState(0)
@@ -26,9 +27,10 @@ function App () {
         pepsi coke hooray! This is a sentence.
       </p>
       <button className='font-sansui bg-red-500 p-2 rounded-lg border-solid hover:bg-red-600 '>Click me</button>
-      <button class='font-sansui bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+      <button className='font-sansui bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
         Button
       </button>
+      <Score />
     </>
   )
 }
---
+++ src/assets/components/Score.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export default function Score () {
+    const [scores, setScores] = useState([]);
+
+    useEffect(() => {
+        console.log("useEffect");
+        fetch("http://127.0.0.1:3001/topPlayer")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setScores(data);
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
+
+    function createScores(){
+        
+    }
+
+    return (
+        <>
+            <p>I'm a score component -{5 + 5}-</p>
+        </>
+    );
+}
