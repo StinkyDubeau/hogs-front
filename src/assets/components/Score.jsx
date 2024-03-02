a552d6e733cd162926861018f688f780a717895d
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-02 17:51:50 -0500

    Fix formatting

--- src/assets/components/Score.jsx
+++ src/assets/components/Score.jsx
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 export default function Score () {
-    const [scores, setScores] = useState([]);
+    const [scores, setScores] = useState([])
 
     useEffect(() => {
-        console.log("useEffect");
-        fetch("http://127.0.0.1:3001/topPlayer")
+        console.log('useEffect')
+        fetch('http://127.0.0.1:3001/topPlayer')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                setScores(data);
+                console.log(data)
+                setScores(data)
             })
             .catch(err => {
-                console.log(err.message);
-            });
-    });
+                console.log(err.message)
+            })
+    })
 
-    function createScores(){
-        
-    }
+    function createScores () {}
 
     return (
         <>
             <p>I'm a score component -{5 + 5}-</p>
         </>
-    );
+    )
 }
