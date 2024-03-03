17ea431abed676194ec608b07965c279497786b3
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-02 21:13:01 -0500

    Get scores loading successfully. Broke prettier, though ): All in due time...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Score from './assets/components/Score.jsx'
 
 function App () {
-    const [count, setCount] = useState(0)
-
     return (
         <>
             <h1 className='font-tourmaline text-3xl'>Tourmaline</h1>
--- src/assets/components/Score.jsx
+++ src/assets/components/Score.jsx
@@ -4,10 +4,14 @@ export default function Score () {
     const [scores, setScores] = useState([])
 
     useEffect(() => {
-        fetch('http://127.0.0.1:3001/test')
+        fetch('http://127.0.0.1:3001/api/scores', {
+            method: 'get',
+            headers: new Headers({
+                API_KEY: 'this is a very insecure api key',
+            })
+        })
             .then(response => response.json())
             .then(data => {
-                console.log(data)
                 setScores(data)
             })
             .catch(err => {
@@ -15,12 +19,31 @@ export default function Score () {
             })
     }, [])
 
-    function createScores () {}
+    function createScore(score) {
+        return (
+            <div className='text-left bg-slate-500 p-2 my-2 px-8 text-slate-800 border-slate-700 border-b-4 rounded-lg'>
+                <p>User ID: {score.user_id}</p>
+                <p>Map: {score.level}</p>
+                <p>Points: {score.points}</p>
+                <p>Time: {score.time}</p>
+                <p>Gamemode: {score.game_mode}</p>
+                <p>Game version: {score.game_version}</p>
+                <p className="text-xs">Unique Score ID: {score._id}</p>           
+            </div>
+        )
+    }
 
     return (
         <>
             <p>I'm a score component -{5 + 5}-</p>
-            <p>Today's date is {scores.time}</p>
+
+            //Show all of the scores returned from the server. Display them all as simple cards for now.
+            {scores.map(createScore)}
+
+            <div className='text-left bg-slate-500 p-2 px-8 text-slate-800 border-slate-700 border-b-4 shadow-lg rounded-lg'>
+                <p className='text-slate-300'>There were {scores.length} responses from the server.</p>
+                <p>{JSON.stringify(scores)}</p>
+            </div>
         </>
     )
 }
