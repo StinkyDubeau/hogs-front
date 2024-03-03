71009352a1de4f95f7317a7e04475d41730b0626
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-02 20:15:10 -0500

    Get front and back API connected successfully!

--- src/assets/components/Score.jsx
+++ src/assets/components/Score.jsx
@@ -4,8 +4,7 @@ export default function Score () {
     const [scores, setScores] = useState([])
 
     useEffect(() => {
-        console.log('useEffect')
-        fetch('http://127.0.0.1:3001/topPlayer')
+        fetch('http://127.0.0.1:3001/test')
             .then(response => response.json())
             .then(data => {
                 console.log(data)
@@ -14,13 +13,14 @@ export default function Score () {
             .catch(err => {
                 console.log(err.message)
             })
-    })
+    }, [])
 
     function createScores () {}
 
     return (
         <>
             <p>I'm a score component -{5 + 5}-</p>
+            <p>Today's date is {scores.time}</p>
         </>
     )
 }
