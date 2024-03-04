1c4eb41181514e5e01895bdc356982d340f4dc66
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-04 08:05:06 -0500

    Start work for rendering background image. TODO: Make foreground blurry

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,34 @@ import './App.css'
 import Score from './assets/components/Score.jsx'
 import Button from "./assets/components/Button.jsx"
 
-function App () {
+function App() {
     return (
         <>
-            <h1 className='font-tourmaline text-3xl'>Tourmaline</h1>
-            <h1 className='font-hogs text-5xl'>H.O.G.S</h1>
-            <p className='font-ipsum text-xs text-justify'>
-                Here is some lorum ipsum text. Blah blah pepsi coke hooray! This
-                is a sentence. Here is some lorum ipsum text. Blah blah pepsi
-                coke hooray! This is a sentence. Here is some lorum ipsum text.
-                Blah blah pepsi coke hooray! This is a sentence. Here is some
-                lorum ipsum text. Blah blah pepsi coke hooray! This is a
-                sentence. Here is some lorum ipsum text. Blah blah pepsi coke
-                hooray! This is a sentence. Here is some lorum ipsum text. Blah
-                blah pepsi coke hooray! This is a sentence. Here is some lorum
-                ipsum text. Blah blah pepsi coke hooray! This is a sentence.
-                Here is some lorum ipsum text. Blah blah pepsi coke hooray! This
-                is a sentence. Here is some lorum ipsum text. Blah blah pepsi
-                coke hooray! This is a sentence. Here is some lorum ipsum text.
-                Blah blah pepsi coke hooray! This is a sentence. Here is some
-                lorum ipsum text. Blah blah pepsi coke hooray! This is a
-                sentence.
-            </p>
-            <Button text="Click me!"/>
-            <Button text="Button"/>
-            <Score />
+            <div className="bg-slate-300 p-5">
+            {/* https://tailwindcss.com/docs/background-image */}
+                <h1 className='font-tourmaline text-3xl'>Tourmaline</h1>
+                <h1 className='font-hogs text-5xl'>H.O.G.S</h1>
+                <p className='font-ipsum text-xs text-justify'>
+                    Here is some lorum ipsum text. Blah blah pepsi coke hooray! This
+                    is a sentence. Here is some lorum ipsum text. Blah blah pepsi
+                    coke hooray! This is a sentence. Here is some lorum ipsum text.
+                    Blah blah pepsi coke hooray! This is a sentence. Here is some
+                    lorum ipsum text. Blah blah pepsi coke hooray! This is a
+                    sentence. Here is some lorum ipsum text. Blah blah pepsi coke
+                    hooray! This is a sentence. Here is some lorum ipsum text. Blah
+                    blah pepsi coke hooray! This is a sentence. Here is some lorum
+                    ipsum text. Blah blah pepsi coke hooray! This is a sentence.
+                    Here is some lorum ipsum text. Blah blah pepsi coke hooray! This
+                    is a sentence. Here is some lorum ipsum text. Blah blah pepsi
+                    coke hooray! This is a sentence. Here is some lorum ipsum text.
+                    Blah blah pepsi coke hooray! This is a sentence. Here is some
+                    lorum ipsum text. Blah blah pepsi coke hooray! This is a
+                    sentence.
+                </p>
+                <Button text="Click me!" />
+                <Button text="Button" />
+                <Score />
+            </div>
         </>
     )
 }
