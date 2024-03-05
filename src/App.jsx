14be42c72b5a56ce75c2bb398723f81344beede4
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-05 10:01:01 -0500

    Tweak glass material

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Leaderboard from './assets/components/Leaderboard.jsx'
 function App() {
     return (
         <>
-            <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-t-2 border-x p-5 rounded-xl">
+            <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-b-[#00000033] shadow-lg border-t-2 border-b border-x p-5 rounded-xl">
                 {/* https://tailwindcss.com/docs/background-image */}
                 <h1 className='font-tourmaline text-3xl'>Tourmaline</h1>
                 <h1 className='font-hogs text-5xl'>H.O.G.S</h1>
