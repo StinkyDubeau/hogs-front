d4242c696861123c0a7f88de9395afdbab5afbed
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-05 09:51:59 -0500

    Fix button animation

--- src/assets/components/Button.jsx
+++ src/assets/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button(props) {
     return (
-        <button className='font-sansui m-1 bg-red-500 p-2 border-b-4 border-red-700 rounded-lg border-solid w-28 hover:bg-red-400 hover:w-36 hover:transition-all focus:border-none'>
+        <button className='font-sansui m-1 bg-red-500 p-2 border-b-4 border-red-700 rounded-lg border-solid w-28 hover:bg-red-400 hover:w-36 transition-all'>
             <p>{props.text}</p>
         </button>
     );
