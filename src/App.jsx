35d7543f3812352c9132be021350497279d7beb1
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-07 07:52:00 -0500

    Move test page to it's own component to prepare for multi-page site

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
 import './App.css'
 import Button from "./components/Button.jsx"
 import Leaderboard from './components/Leaderboard.jsx'
+import Test from "./pages/Test.jsx"
 
 function App() {
     return (
         <>
-            <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-b-[#00000033] shadow-lg border-t-2 border-b border-x p-5 rounded-xl">
-                {/* https://tailwindcss.com/docs/background-image */}
-                <h1 className='font-tourmaline text-3xl'>Tourmaline</h1>
-                <h1 className='font-hogs text-5xl'>H.O.G.S</h1>
-                <p className='font-ipsum text-xs text-justify'>
-                    Here is some lorum ipsum text. Blah blah pepsi coke hooray! This
-                    is a sentence. Here is some lorum ipsum text. Blah blah pepsi
-                    coke hooray! This is a sentence. Here is some lorum ipsum text.
-                    Blah blah pepsi coke hooray! This is a sentence. Here is some
-                    lorum ipsum text. Blah blah pepsi coke hooray! This is a
-                    sentence. Here is some lorum ipsum text. Blah blah pepsi coke
-                    hooray! This is a sentence. Here is some lorum ipsum text. Blah
-                    blah pepsi coke hooray! This is a sentence. Here is some lorum
-                    ipsum text. Blah blah pepsi coke hooray! This is a sentence.
-                    Here is some lorum ipsum text. Blah blah pepsi coke hooray! This
-                    is a sentence. Here is some lorum ipsum text. Blah blah pepsi
-                    coke hooray! This is a sentence. Here is some lorum ipsum text.
-                    Blah blah pepsi coke hooray! This is a sentence. Here is some
-                    lorum ipsum text. Blah blah pepsi coke hooray! This is a
-                    sentence.
-                </p>
-                <Button text="Click me!" />
-                <Button text="Button" />
-                <Leaderboard />
-            </div>
+            <h1>hello</h1>
+            <Test />
         </>
     )
 }
