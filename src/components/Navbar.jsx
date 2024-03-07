3b46c34d9089118594e5e87b3764288cafc98b5d
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-07 14:15:31 -0500

    Tune glass material

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 export default function Navbar() {
     return (
-        <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-b-[#00000033] shadow-lg border-t-2 border-b mb-5 border-x p-5 rounded-xl">
+        <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-b-[#00000033] shadow-lg border-t-2 border-b-2 mb-5 border-x p-5 rounded-xl">
             <p>Logo</p>
             <Link to="/"><Button text="Home"/></Link>
             <Link to="/test"><Button text="Test"/></Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <>
-            <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-b-[#00000033] shadow-lg border-t-2 border-b border-x p-5 rounded-xl">
+            <div className="text-slate-200 backdrop-blur-xl border-white border-t-[#ffffff33] border-x-[#ffffff22] border-b-[#00000033] shadow-lg border-t-2 border-b-2 border-x p-5 rounded-xl">
                 <h1>Hello! This is the homepage</h1>
                 <p>It's only really here to test multi-page rendering.</p>
                 <p>There's a navbar above, which is only here for hopping between pages during this testing phase. You'll find the community page has big margins around the cork: that's a side-effect of the navbar and isn't intended to be there long-term.</p>
