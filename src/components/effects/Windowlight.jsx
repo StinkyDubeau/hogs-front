c1029e397654babe3711b6095863be5f4f840c7d
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-07 12:42:48 -0500

    Add brightness variable to window component

--- src/components/effects/Windowlight.jsx
+++ src/components/effects/Windowlight.jsx
@@ -1,11 +1,12 @@
-export default function Windowlight() {
+export default function Windowlight(props) {
     return (
-        <div className="lighting-fx blur-md z-50 fixed mix-blend-overlay pointer-events-none">
+        // Brightness (opacity) from 0 to 100
+        <div className={`lighting-fx blur-md opacity-${props.brightness} z-50 fixed mix-blend-overlay pointer-events-none`}>
             <div className="relative mix left-[300px] top-[100px] grid gap-4 grid-cols-2">
-                <div className="h-48 w-24 bg-slate-200">1</div>
-                <div className="h-48 w-24 bg-white">2</div>
-                <div className="h-48 w-24 bg-white">3</div>
-                <div className="h-48 w-24 bg-white">4</div>
+                <div className="h-48 w-24 bg-white"></div>
+                <div className="h-48 w-24 bg-white"></div>
+                <div className="h-48 w-24 bg-white"></div>
+                <div className="h-48 w-24 bg-white"></div>
             </div>
         </div>
     )
