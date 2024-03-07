80f82a1d82e27532dc448f7c08972c4a76c94cf8
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-07 13:04:33 -0500

    Add react player for video backgrounds. Implement test on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,20 @@
-export default function About(){
-    return(
-    <>
-    <div className="text-slate-200">
-        <p>About page</p>
-    </div>
-    </>
+import ReactPlayer from "react-player";
+
+export default function About() {
+    return (
+        <>
+            <div className="text-slate-200">
+                <ReactPlayer
+                className="-z-50"
+                url="https://samplelib.com/lib/preview/mp4/sample-20s.mp4"
+                playing muted loop
+                width="100%"
+                height="100%"
+                />
+                <p>About page</p>
+                <img></img>
+                <h1 className="text-3xl font-sansui tracking-widest">A reimagined classic</h1>
+            </div>
+        </>
     );
 }
