ef3eed0fb3bb1546917da5587b7967b74194ac12
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-10 18:24:05 -0400

    Add logo to navbar

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button(props) {
     return (
-        <button className="m-1 w-28 rounded-lg border-b-2 border-solid border-slate-700 bg-slate-500 p-2 font-sansui transition-all hover:w-36 hover:bg-slate-400">
+        <button className="font-sansui m-1 bg-slate-500 p-2 border-b-4 border-slate-700 rounded-lg border-solid w-28 hover:bg-slate-400 hover:w-36 transition-all">
             <p>{props.text}</p>
         </button>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,33 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
 import Glass from "../components/materials/Glass"
+import Logo from "../assets/StateSingularIcon.png";
 
 export default function Navbar() {
     return (
         <Glass>
-            <p>Logo</p>
-            <Link to="/">
-                <Button text="Home" />
-            </Link>
-            <Link to="/test">
-                <Button text="Test" />
-            </Link>
-            <Link to="/about">
-                <Button text="About" />
-            </Link>
-            <Link to="/community">
-                <Button text="Community" />
-            </Link>
-            <Link to="/leaderboards">
-                <Button text="Leaderboard" />
-            </Link>
+            <div className="site-logo flex justify-center mb-3">
+                <button>
+                    <img className="hover:scale-110 max-h-12 transition-all" src={Logo} alt="studio logo"/>
+                </button>
+            </div>
+            <div className="flex justify-center">
+                <Link to="/">
+                    <Button text="Home" />
+                </Link>
+                <Link to="/test">
+                    <Button text="Test" />
+                </Link>
+                <Link to="/about">
+                    <Button text="About" />
+                </Link>
+                <Link to="/community">
+                    <Button text="Community" />
+                </Link>
+                <Link to="/leaderboards">
+                    <Button text="Leaderboard" />
+                </Link>
+            </div>
         </Glass>
     );
 }
