8554487717d8bf8e87ef2818b9cf35624a557859
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-10 18:26:34 -0400

    Remove 0th place from leaderboards

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -27,7 +27,7 @@ export default function Leaderboard(props) {
     function createRow(score, index) {
         return (
             <tr key={score._id} className="m-2 scale-95 rounded-lg border-slate-700 transition-all hover:scale-100 hover:bg-slate-500 hover:shadow-lg">
-                <td>{index}</td>
+                <td>{index+1}</td>
                 <td>{score.user_id}</td>
                 <td>{score.level}</td>
                 <td>{score.points}</td>
