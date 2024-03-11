e031e264674ba99d4ff635e1adb3a06504ad4623
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-10 20:50:42 -0400

    Add filter ui for leaderboards

--- src/pages/Leaderboards.jsx
+++ src/pages/Leaderboards.jsx
@@ -9,13 +9,15 @@ export default function Leaderboards() {
             <div className="md:flex flex-wrap gap-2">
                 <Glass className="">
                     <div className="md:flex-col flex gap-2 justify-center md:max-w-xs">
-                        <span className="text-slate-300 material-symbols-outlined">
-                            filter_alt
-                        </span>
                         <div className="">
+                            <span className="text-slate-300 material-symbols-outlined">
+                                filter_alt
+                            </span>
+
                             <input type="text" placeholder="User" className="my-2 p-5 rounded-xl input input-bordered w-full max-w-xs" />
                             <input type="text" placeholder="Level" className="p-5 rounded-xl input input-bordered w-full max-w-xs" />
                         </div>
+
                         <Glass className="input input-bordered h-full">
                             <span class="text-slate-300 material-symbols-outlined">
                                 swap_vert
@@ -29,11 +31,14 @@ export default function Leaderboards() {
                                 <input type="radio" name="radio-10" className="radio checked:bg-slate-400" checked />
                             </div>
                         </Glass>
+
                         <Button text="Search"></Button>
                     </div>
+
+
                 </Glass>
                 {/* Assign a max-h-96 value here to cap the height of the leaderboard */}
-                <Glass className="flex-1 overflow-scroll scrollbar-hide">
+                <Glass className="flex-1 max-h-[400px] overflow-scroll scrollbar-hide">
                     <Leaderboard />
                 </Glass>
 
