696c09ef68d3c74b030f3026b71278e383e18159
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-14 08:44:43 -0400

    Simplify CornerNav

--- src/components/CornerNav.jsx
+++ src/components/CornerNav.jsx
@@ -6,11 +6,10 @@ export default function CornerNav(props) {
     return (
         <div className="fixed left-1.5 top-1 z-50">
             <Glass materialOnly className="rounded-xl">
-                <div className="mx-0.5 mt-0.5 flex">
-
+                <div className="">
                     <Link className="" to="/">
                         <Button className="h-12 w-12">
-                            <span className="absolute right-2.5 material-symbols-outlined mt-1">
+                            <span className="material-symbols-outlined mt-1 text-slate-300">
                                 home
                             </span>
                         </Button>
@@ -18,7 +17,7 @@ export default function CornerNav(props) {
 
                     <Link className="" to="community">
                         <Button className="h-12 w-12">
-                            <span className="material-symbols-outlined mt-1">
+                            <span className="material-symbols-outlined mt-1 text-slate-300">
                                 communities
                             </span>
                         </Button>
@@ -26,7 +25,7 @@ export default function CornerNav(props) {
 
                     <Link className="" to="news">
                         <Button className="h-12 w-12">
-                            <span className="material-symbols-outlined mt-1">
+                            <span className="material-symbols-outlined mt-1 text-slate-300">
                                 news
                             </span>
                         </Button>
@@ -34,7 +33,7 @@ export default function CornerNav(props) {
 
                     <Link className="" to="About">
                         <Button className="h-12 w-12">
-                            <span className="material-symbols-outlined mt-1">
+                            <span className="material-symbols-outlined mt-1 text-slate-300">
                                 help
                             </span>
                         </Button>
