1518f5b2a115f17ce3a6e3798a6ad831dd3a38de
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-21 14:23:04 -0400

    Remove windowlight brightness property due to bugginess

--- src/components/effects/Windowlight.jsx
+++ src/components/effects/Windowlight.jsx
@@ -2,7 +2,7 @@ export default function Windowlight(props) {
     return (
         // Brightness (opacity) from 0 to 100
         <div
-            className={`lighting-fx blur-md opacity-${props.brightness} pointer-events-none fixed z-30 mix-blend-overlay`}
+            className={`lighting-fx blur-md opacity-50 pointer-events-none fixed z-30 mix-blend-overlay`}
         >
             <div className="mix relative left-[300px] top-[80px] grid grid-cols-2 gap-8">
                 <div className="h-80 w-48 bg-white"></div>
