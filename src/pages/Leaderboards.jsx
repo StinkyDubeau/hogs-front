26adeb5fe0090af56e1da83f61bc140cccb6309d
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-21 12:42:10 -0400

    Fixed bug where lighting layer was above nav buttons

--- src/pages/Leaderboards.jsx
+++ src/pages/Leaderboards.jsx
@@ -13,16 +13,28 @@ export default function Leaderboards() {
 
     const [trigger, setTrigger] = useState(0);
 
+    const [showCheaters, setShowCheaters] = useState(false);
+    const [showOld, setShowOld] = useState(false);
     const [showAdvanced, setShowAdvanced] = useState(false);
+
+    function handleClickAdvanced() {
+        showAdvanced ? setShowAdvanced(false) : setShowAdvanced(true);
+    }
+
+    function handleSearch() {
+        console.log("Refreshing results.");
+        !showCheaters ? setGamemode("story") : setGamemode("");
+        !showOld ? setGameversion("0.2.0") : setGameversion("");
+        setTrigger(trigger + 1);
+    }
 
     function createSummary() {
         return (
             <ul>
-                <p>Sort by: {gamemode}</p>
-                <p>Filter user: {userid}</p>
-                <p>Filter level: {level}</p>
-                <p>Sort by: {gamemode}</p>
-                <p>Sort by: {gamemode}</p>
+                <li className="flex justify-between"><p>Sort by:</p><p>{sortBy}</p></li>
+                <li className="flex justify-between"><p>Filter user:</p><p>{userid}</p></li>
+                <li className="flex justify-between"><p>Filter level:</p><p>{level}</p></li>
+                <li className="flex justify-between"><p>Query no:</p><p>{trigger}</p></li>
             </ul>
         );
     }
@@ -31,9 +43,10 @@ export default function Leaderboards() {
         if (showAdvanced) {
             return (
                 <Glass className="p-6">
+                    <p className="font-sansui text-2xl mb-2">Advanced Search</p>
                     <Glass className="bg-zinc-800">
-                        <p className="font-sansui text-lg">Active params:</p>
-                        <p className="mb-4 font-sansui">{createSummary()}</p>
+                        <p className="font-sansui text-xl">Active params:</p>
+                        <p className="font-sansui text-left">{createSummary()}</p>
                     </Glass>
 
                     <Glass className="h-full bg-zinc-800 px-8">
@@ -41,7 +54,12 @@ export default function Leaderboards() {
                             <p>Show cheaters</p>
                             <input
                                 type="checkbox"
-                                checked="checked"
+                                checked={showCheaters && "checked"}
+                                onChange={(e) =>
+                                    showCheaters
+                                        ? setShowCheaters(!e.target.value)
+                                        : setShowCheaters(e.target.value)
+                                }
                                 className="checkbox"
                             />
                         </div>
@@ -49,7 +67,12 @@ export default function Leaderboards() {
                             <p>Show old versions</p>
                             <input
                                 type="checkbox"
-                                checked="checked"
+                                checked={showOld && "checked"}
+                                onChange={(e) =>
+                                    showOld
+                                        ? setShowOld(!e.target.value)
+                                        : setShowOld(e.target.value)
+                                }
                                 className="checkbox"
                             />
                         </div>
@@ -65,100 +88,83 @@ export default function Leaderboards() {
                 </Glass>
             );
         } else {
-            return <></>;
+            return (
+                <div onClick={handleClickAdvanced}>
+                    <Button
+                        className="bg-zinc-800 hover:bg-zinc-700"
+                        text="Show advanced"
+                    />
+                </div>
+            );
         }
-    }
-
-    function handleClickAdvanced() {
-        showAdvanced ? setShowAdvanced(false) : setShowAdvanced(true);
-    }
-
-    function handleSearch() {
-        console.log("Refreshing results.");
-        setTrigger(trigger + 1);
     }
 
     return (
         <Frame noNavbar>
             <div className="mt-12 flex-wrap gap-2 md:flex">
                 <div className="md:max-w-xs">
-
-                        <div className="justify-center gap-2  md:flex-col">
-                            <div className=" gap-2 md:inline">
-                                <Glass>
-                                    <span className="material-symbols-outlined text-slate-300">
-                                        swap_vert
+                    <div className="justify-center gap-2  md:flex-col">
+                        <div className=" gap-2 md:inline">
+                            <Glass>
+                                <span className="material-symbols-outlined text-slate-300">
+                                    swap_vert
+                                </span>
+                                <div className="my-2 flex justify-between">
+                                    <span className="font-sansui text-slate-300">
+                                        Points
                                     </span>
-                                    <div className="my-2 flex justify-between">
-                                        <span className="font-sansui text-slate-300">
-                                            Points
-                                        </span>
-                                        <input
-                                            type="radio"
-                                            name="radio-10"
-                                            className="radio checked:bg-slate-300"
-                                            onClick={() => setSortBy("points")}
-                                        />
-                                    </div>
-                                    <div className="my-2 flex justify-between">
-                                        <span className="font-sansui text-slate-300">
-                                            Time
-                                        </span>
-                                        <input
-                                            type="radio"
-                                            name="radio-10"
-                                            className="radio checked:bg-slate-300"
-                                            onClick={() => setSortBy("time")}
-                                        />
-                                    </div>
-                                </Glass>
-                                <Glass className="h-full">
-                                    <div>
-                                        <span className="material-symbols-outlined text-slate-300">
-                                            filter_alt
-                                        </span>
-                                    </div>
                                     <input
-                                        type="text"
-                                        placeholder="User"
-                                        value={userid}
-                                        className="my-2 h-12 w-full max-w-xs rounded-xl bg-[#00000044] shadow-inner-xl p-5 font-sansui"
-                                        onChange={(e) =>
-                                            setUserid(e.target.value)
-                                        }
+                                        type="radio"
+                                        name="radio-10"
+                                        className="radio checked:bg-slate-300"
+                                        onClick={() => setSortBy("points")}
                                     />
+                                </div>
+                                <div className="my-2 flex justify-between">
+                                    <span className="font-sansui text-slate-300">
+                                        Time
+                                    </span>
                                     <input
-                                        type="text"
-                                        placeholder="Level"
-                                        value={level}
-                                        className="h-12 w-full max-w-xs rounded-xl bg-[#00000044] shadow-inner-xl p-5 font-sansui"
-                                        onChange={(e) =>
-                                            sortLevel(e.target.value)
-                                        }
-                                        onSubmit={handleSearch}
+                                        type="radio"
+                                        name="radio-10"
+                                        className="radio checked:bg-slate-300"
+                                        onClick={() => setSortBy("time")}
                                     />
-                                    <div
-                                        className="mt-2 flex justify-center"
-                                        onClick={handleSearch}
-                                    >
-                                        <Button
-                                            className="w-32"
-                                            text="Filter"
-                                        ></Button>
-                                    </div>
-                                </Glass>
-                            </div>
-
-                            <div
-                                onClick={handleClickAdvanced}
-                                className={() => showAdvanced && "hidden"}
-                            >
-                                <Button
-                                    className="bg-zinc-800 hover:bg-zinc-700"
-                                    text="Advanced"
+                                </div>
+                            </Glass>
+                            <Glass className="h-full">
+                                <div>
+                                    <span className="material-symbols-outlined text-slate-300">
+                                        filter_alt
+                                    </span>
+                                </div>
+                                <input
+                                    type="text"
+                                    placeholder="User"
+                                    value={userid}
+                                    className="shadow-inner-xl my-2 h-12 w-full max-w-xs rounded-xl bg-[#00000044] p-5 font-sansui"
+                                    onChange={(e) => setUserid(e.target.value)}
                                 />
-                            </div>
+                                <input
+                                    type="text"
+                                    placeholder="Level"
+                                    value={level}
+                                    className="shadow-inner-xl h-12 w-full max-w-xs rounded-xl bg-[#00000044] p-5 font-sansui"
+                                    onChange={(e) => sortLevel(e.target.value)}
+                                    onSubmit={handleSearch}
+                                />
+                                <div
+                                    className="mt-2 flex justify-center"
+                                    onClick={handleSearch}
+                                >
+                                    <Button
+                                        className="w-32"
+                                        text="Filter"
+                                    ></Button>
+                                </div>
+                            </Glass>
                         </div>
+                    </div>
 
                     <div>{createAdvanced()}</div>
                 </div>
@@ -170,6 +176,7 @@ export default function Leaderboards() {
                         level={level}
                         game_mode={gamemode}
                         sort_by={sortBy}
+                        game_version={gameversion}
                     />
                 </Glass>
             </div>
