57c9d24ea6ca18d61f125fe6ac1aa5328371bd40
StinkyDubeau <jakersunderscore09@gmail.com>
2024-03-21 10:30:06 -0400

    Start to add reactivity to community page. Add trailer assetrailer asset

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -7,24 +7,32 @@ export default function Community() {
         <Frame noNavbar noPad>
             <div className="h-screen overflow-hidden bg-amber-800 bg-cork shadow-inner-4xl">
                 <Windowlight brightness="50" />
-                <div className="mt-[25vh] flex flex-auto justify-center gap-20">
-                    <Polaroid className="-rotate-3" text="No image" to="/" />
-
-                    <Polaroid
-                        className="rotate-3"
-                        text="Four"
-                        to="/"
-                        img="./test-image-one.jpg"
-                        alt="the number four painted on a white brick wall"
-                    />
-
-                    <Polaroid
-                        className="rotate-6"
-                        text="Leaderboards"
-                        to="/leaderboards"
-                        img="./hogs-background.png"
-                        alt="hogs gameplay"
-                    />
+                <div className="flex flex-wrap justify-center mt-[1/2vh]">
+                    <div className="border-2 border-black bg-red-300">
+                        <Polaroid
+                            className="-rotate-3"
+                            text="No image"
+                            to="/"
+                        />
+                    </div>
+                    <div className="border-2 border-black bg-red-300">
+                        <Polaroid
+                            className="rotate-3"
+                            text="Four"
+                            to="/"
+                            img="./test-image-one.jpg"
+                            alt="the number four painted on a white brick wall"
+                        />
+                    </div>
+                    <div className="border-2 border-black bg-red-300">
+                        <Polaroid
+                            className="rotate-6"
+                            text="Leaderboards"
+                            to="/leaderboards"
+                            img="./hogs-background.png"
+                            alt="hogs gameplay"
+                        />
+                    </div>
                 </div>
             </div>
         </Frame>
