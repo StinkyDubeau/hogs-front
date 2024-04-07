394a2728a052be119e240328875df34245c1137e
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-07 19:23:11 -0400

    Add news post component to writer page

--- src/pages/Writer.jsx
+++ src/pages/Writer.jsx
@@ -1,5 +1,6 @@
 import Frame from "../components/Frame";
 import Glass from "../components/materials/Glass";
+import NewsPost from "../components/NewsPost";
 import { useState } from "react";
 
 export default function Writer(props) {
@@ -25,12 +26,7 @@ export default function Writer(props) {
                 <p>This is the blog writer page.</p>
             </Glass>
             <Glass>
-            <p className="text-left font-sansui text-3xl">{post.title}</p>
-                <p className="text-left font-sansui text-xl">{post.author}</p>
-                <p className="text-left font-sansui text-lg">{post.body}</p>
-                <p className="text-right font-sansui text-xs text-slate-400">
-                    id: {post._id}
-                </p>
+                {NewsPost(post)}
             </Glass>
         </Frame>
     );
