098d3b80df84a822b9169ba53788df6b37453860
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-07 20:32:41 -0400

    Add 'are you sure?' screen,  and add embed field

--- src/pages/Writer.jsx
+++ src/pages/Writer.jsx
@@ -1,22 +1,46 @@
 import Frame from "../components/Frame";
 import Glass from "../components/materials/Glass";
 import NewsPost from "../components/NewsPost";
+import Button from "../components/Button";
 import { useState } from "react";
 
 export default function Writer(props) {
     const now = new Date().toLocaleString();
 
+    const [verifying, setVerifying] = useState(false);
     const [post, setPost] = useState({
         title: "",
         author: "",
         abstract: "",
+        embed: "",
         date: now,
         body: "",
         _id: "1234-abcd-5678-efgh",
     });
 
-    return (
-        <Frame noCornerNav>
+    function createVerify() {
+        console.log("CREATE VERIFY");
+        return (
+            <Glass>
+                <p className="font-sansui text-3xl">Are you sure?</p>
+                <p className="mb-2 font-sansui text-red-400">
+                    Once you click submit, the only way to delete/modify the
+                    post is with direct API access.
+                </p>
+                <div className="flex justify-center">
+                    <div onClick={() => setVerifying(false)}>
+                        <Button>Back</Button>
+                    </div>{" "}
+                    <div onClick={() => setVerifying(false)}>
+                        <Button>Submit</Button>
+                    </div>{" "}
+                </div>
+            </Glass>
+        );
+    }
+
+    function createForm() {
+        return (
             <Glass>
                 <p className="mb-2 text-left font-sansui text-3xl">
                     Create a new news post
@@ -25,7 +49,6 @@ export default function Writer(props) {
                     Use this page for data insertion, not for writing. Your
                     changes will not be saved if the page is refreshed.
                 </p>
-
                 <div className="flex flex-wrap justify-center gap-x-6">
                     {/* Title */}
                     <div className="flex flex-wrap gap-2 object-center">
@@ -75,6 +98,22 @@ export default function Writer(props) {
                             }
                         />
                     </div>
+                    {/* Embed */}
+                    <div className="flex flex-wrap gap-2 object-center">
+                        <p className="mt-5 font-sansui">Embed</p>
+                        <input
+                            type="text"
+                            placeholder="Embed url"
+                            value={post.embed}
+                            className="my-2 h-12 rounded-xl bg-[#00000044] p-5 font-sansui shadow-inner-xl"
+                            onChange={(e) =>
+                                setPost({
+                                    ...post,
+                                    embed: e.target.value,
+                                })
+                            }
+                        />
+                    </div>
                     {/* Body */}
                     <div className="w-100 flex flex-wrap gap-2 object-center">
                         <p className="mt-5 font-sansui">Body</p>
@@ -82,7 +121,7 @@ export default function Writer(props) {
                             type="text"
                             placeholder="Body"
                             value={post.body}
-                            className="my-2 rounded-xl bg-[#00000044] p-5 font-sansui shadow-inner-xl"
+                            className="my-2 h-12 rounded-xl bg-[#00000044] p-5 font-sansui shadow-inner-xl"
                             onChange={(e) =>
                                 setPost({
                                     ...post,
@@ -92,7 +131,19 @@ export default function Writer(props) {
                         />
                     </div>
                 </div>
+                <div className="flex justify-center">
+                    <div className="w-24" onClick={() => setVerifying(true)}>
+                        <Button className="w-24">Submit</Button>
+                    </div>
+                </div>
             </Glass>
+        );
+    }
+
+    return (
+        <Frame noCornerNav>
+            {/* Form */}
+            {verifying ? createVerify() : createForm()}
 
             {/* Post Preview */}
             <Glass>
