e9aec8b0e80d3893fbb4508d5097d6747021936a
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-04-17 13:05:41 -0400

    Remove links and references to server functions, though they are still available in-router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import HomeGif3 from "../assets/home-gif-3.gif";
 
 export default function Home() {
     return (
-        <Frame noNavbar>
+        <Frame noNavbar noCornerNav>
             <div className="mt-0 sm:mt-12" />
             <Glass>
                 <div className="relative max-h-[550px] overflow-hidden rounded-lg bg-zinc-600 shadow-inner-3xl">
@@ -58,8 +58,15 @@ export default function Home() {
                         </Button>
                     </div>
                 </div>
-                <div className="my-5">
+                {/* COMMUNITY TICKER -- DISABLED IN SERVERLESS BRANCH */}
+                {/* <div className="my-5">
                     <CommunityTicker />
+                </div> */}
+            </Glass>
+
+            <Glass>
+                <div className="mt-5">
+                    <About />
                 </div>
                 <p className="mt-8 font-sansui text-2xl text-slate-50">
                     Scroll to learn more
@@ -68,12 +75,6 @@ export default function Home() {
                     expand_more
                 </span>
             </Glass>
-
-            <div className="mt-20" />
-
-            <div id="about" className="my-20">
-                <About />
-            </div>
 
             <Glass
                 id="nonlinear"
@@ -152,6 +153,37 @@ export default function Home() {
                         alt="An animated gif showing fast-paced gameplay in HOGs"
                     ></img>
                 </div>
+            </Glass>
+
+            <Glass>
+                <Button className="bg-[#a53b6781] backdrop-blur-xl hover:scale-125 hover:bg-[#a34091]">
+                    <a
+                        href="https://store.steampowered.com/app/2899270/HOGS/"
+                        alt="Visit HOGs' Steam Page"
+                        target="_blank"
+                    >
+                        <div className="flex gap-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                className="mt-1.5"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M.329 10.333A8.01 8.01 0 0 0 7.99 16C12.414 16 16 12.418 16 8s-3.586-8-8.009-8A8.006 8.006 0 0 0 0 7.468l.003.006 4.304 1.769A2.2 2.2 0 0 1 5.62 8.88l1.96-2.844-.001-.04a3.046 3.046 0 0 1 3.042-3.043 3.046 3.046 0 0 1 3.042 3.043 3.047 3.047 0 0 1-3.111 3.044l-2.804 2a2.223 2.223 0 0 1-3.075 2.11 2.22 2.22 0 0 1-1.312-1.568L.33 10.333Z" />
+                                <path d="M4.868 12.683a1.715 1.715 0 0 0 1.318-3.165 1.7 1.7 0 0 0-1.263-.02l1.023.424a1.261 1.261 0 1 1-.97 2.33l-.99-.41a1.7 1.7 0 0 0 .882.84Zm3.726-6.687a2.03 2.03 0 0 0 2.027 2.029 2.03 2.03 0 0 0 2.027-2.029 2.03 2.03 0 0 0-2.027-2.027 2.03 2.03 0 0 0-2.027 2.027m2.03-1.527a1.524 1.524 0 1 1-.002 3.048 1.524 1.524 0 0 1 .002-3.048" />
+                            </svg>
+                            <p className="text-2xl text-slate-200">
+                                Wishlist Today
+                            </p>
+                        </div>
+                    </a>
+                </Button>
+                <p className="font-sansui">
+                    H.O.G.S is in development, due to release for the 2024
+                    holiday season.
+                </p>
             </Glass>
 
             <div id="steam_embed" className="my-10 flex justify-center">
