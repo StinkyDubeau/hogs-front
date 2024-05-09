1b102df4754b450f0249d9037370b474da1abac1
Jacob Dubeau <jakersunderscore09@gmail.com>
2024-05-09 17:04:45 -0400

    Continue to work on homepage stlying

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,16 @@ export default function Home() {
     return (
         <Frame noNavbar noCornerNav noPad>
             {/* Video Hero */}
-            <div className="h-screen rounded-lg bg-zinc-600 shadow-inner-3xl">
-                <video muted autoPlay playsInline loop>
+            <div className="h-screen rounded-lg bg-slate-200 text-slate-200 shadow-inner-4xl">
+                <video
+                    className="min-h-screen object-cover"
+                    muted
+                    autoPlay
+                    playsInline
+                    loop
+                >
                     <source src={Trailer} type="video/mp4" />
                 </video>
-
                 <div
                     id="CallToAction"
                     className="absolute inset-12 flex flex-col items-center justify-center"
@@ -45,9 +50,7 @@ export default function Home() {
                                             <path d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
                                         </svg>
                                     </div>
-                                    <p className="text-2xl text-slate-200">
-                                        Join the Discord
-                                    </p>
+                                    <p className="text-2xl">Join the Discord</p>
                                 </div>
                             </a>
                         </Button>
@@ -71,17 +74,25 @@ export default function Home() {
                                             <path d="M4.868 12.683a1.715 1.715 0 0 0 1.318-3.165 1.7 1.7 0 0 0-1.263-.02l1.023.424a1.261 1.261 0 1 1-.97 2.33l-.99-.41a1.7 1.7 0 0 0 .882.84Zm3.726-6.687a2.03 2.03 0 0 0 2.027 2.029 2.03 2.03 0 0 0 2.027-2.029 2.03 2.03 0 0 0-2.027-2.027 2.03 2.03 0 0 0-2.027 2.027m2.03-1.527a1.524 1.524 0 1 1-.002 3.048 1.524 1.524 0 0 1 .002-3.048" />
                                         </svg>
                                     </div>
-                                    <p className="text-2xl text-slate-200">
-                                        Wishlist Today
-                                    </p>
+                                    <p className="text-2xl">Wishlist Today</p>
                                 </div>
                             </a>
                         </Button>
                     </div>
+                    <a href="#About" className="absolute bottom-0">
+                        <button>
+                            <p className="font-sansui text-2xl">
+                                Scroll to learn more
+                            </p>
+                            <span className="material-symbols-outlined mt-0 text-4xl text-slate-300">
+                                expand_more
+                            </span>
+                        </button>
+                    </a>
                 </div>
             </div>
 
-            <div className="m-auto max-w-[1280px] p-2 sm:p-6">
+            <div id="About" className="m-auto max-w-[1280px] p-2 sm:p-6">
                 {/* About section */}
                 <Glass className="my-5">
                     <FadeInSection>
@@ -94,12 +105,9 @@ export default function Home() {
                     </FadeInSection>
                 </Glass>
 
-                <Glass
-                    id="nonlinear"
-                    className="my-5 justify-between gap-6 md:flex"
-                >
+                <Glass id="nonlinear">
                     <FadeInSection>
-                        <div className="gap-2 md:flex">
+                        <div className="my-5 justify-between gap-6 md:flex">
                             <div>
                                 <p className="text-left font-sansui text-2xl font-bold uppercase text-slate-200">
                                     H.O.G.S is an immersive FPS
@@ -122,12 +130,9 @@ export default function Home() {
                     </FadeInSection>
                 </Glass>
 
-                <Glass
-                    id="immersive FPS"
-                    className="my-5 justify-between gap-6 md:flex"
-                >
+                <Glass id="immersive FPS">
                     <FadeInSection>
-                        <div className="gap-2 md:flex">
+                        <div className="my-5 justify-between gap-6 md:flex">
                             <div className="md:hidden">
                                 {/* ENSURE YOU CHANGE BOTH SECTIONS IF UPDATING THIS TEXT! */}
                                 <p className="text-left font-sansui text-2xl font-bold uppercase text-slate-200">
